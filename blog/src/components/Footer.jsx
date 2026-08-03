20b3ce462b9e9ee376bc206b9b2f7ad24dfe3fda
Krutagna <krutagnapatelcomputereng@gmail.com>
2026-08-03 20:06:18 +0530

    Footer Change

--- blog/src/components/Footer.jsx
+++ blog/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-5">
-      © 2026 React Blog | Created by Krutagna Patel
+      © 2026 React Blog | Created by BlogDen Team. All rights reserved.
     </footer>
   );
 }
